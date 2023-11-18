315df9a023da27a3a6eb07052e580ad29f6a274f
Cj Alexander <clauvinsalexander@gmail.com>
2023-11-18 17:45:10 -0500

    Added functionality for equal passwords

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const password1 = document.querySelector(".pass1")
 const password2 = document.querySelector(".pass2")
 
 const checkpass1 = []
-const checkpass2 = []
 
 password1.addEventListener(('input'), (e) => {
     if (e.data !== null) {
@@ -10,20 +9,6 @@ password1.addEventListener(('input'), (e) => {
     } else {
         checkpass1.pop()
     }
-    
-})
-
-password2.addEventListener(('input'), (e) => {
-    if (e.data !== null) {
-        checkpass2.push(e.data)
-    } else {
-        checkpass2.pop()
-    }
-    // Easiest way to compare array elements
-    if (JSON.stringify(checkpass1) === JSON.stringify(checkpass2)) {
-        password2.setAttribute('class', 'input.pass2:valid')
-    } else {
-        password2.setAttribute('class', 'input.pass2:invalid')
-    }
-    
+    let pattern = checkpass1.join("")
+    password2.setAttribute("pattern", pattern)
 })
